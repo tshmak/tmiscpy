86429f2882bb2022a0807e692cff5971106dbe80
tshmak <timothy@fano.ai>
2020-10-29 14:35:56 +0800

    Updates kaldi_audio

--- tmiscpy/functions.py
+++ tmiscpy/functions.py
@@ -7,9 +7,9 @@ Created on Wed Jul 24 17:08:02 2019
 """
 
 __all__ = ['wavaudio', 'read_mnist', 'examine', 'jiebacut', 'chdir', 
-           'home', 'os', 'head', 'interactive', 'filename', 'kaldi_segment']
+           'home', 'os', 'head', 'interactive', 'filename', 'kaldi_audio']
 import pdb
-import os
+import os, sys
 
 
 """
@@ -30,12 +30,11 @@ def filename(filenamestub):
 """
 Turn a bash pipe into a file like object that can be read by pd.read_csv()
 """
-def pipe2file(cmd):
+def pipe2file(cmd, **kwargs):
     
-    import sys
     import subprocess
     
-    a = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    a = subprocess.Popen(cmd, stdout=subprocess.PIPE, **kwargs)
     
     from io import StringIO
     
@@ -266,105 +265,152 @@ class wavaudio:
         
 
 """
-A class for handling kaldi audio segments
+A class for handling kaldi audio data 
+See https://kaldi-asr.org/doc/data_prep.html for background
+
+It outputs a pandas Series, or a pandas DataFrame if the .df() function is used 
+The DataFrame/Series is indexed by the utterance ID, and can be subsetted using standard 
+pandas syntax. 
+
+The utterance ID (uttID) is taken from the segments file. If the segments file is not present, 
+it comes from the wav.scp file (which must be present), and is equal to the recording ID (recID). 
 
 Usage examples: 
-s = kaldi_segment('/path/to/segment_path')
-s.subset = a_list_of_segmentID
-s.wavID
-s.segmentID
-s.start
-s.df(['wavID', 'start', 'end']).to_csv(header=False, index=False, sep='\t')
+s = kaldi_audio('/path/to/kaldi_data')
+s.uttID                                # Outputs a pandas Series
+s.recID
+s.begin                                # Note that this may not be available.
+s.wavfile 
+s.df(['uttID', 'recID', 'wavfile'])    # Outputs a pandas DataFrame
+s.wavfile.loc[some_uttID_list]         # for subsetting
+s.df(['wavfile', 'begin', 'end']).to_csv(header=False, index=False, sep='\t')
 """
 
 import pandas as pd
-class kaldi_segment: 
+class kaldi_audio: 
     
     def __init__(self, segment_path, 
                  subset = None, 
                  segments = 'segments', 
                  text = 'text', 
-                 wav = 'wav.scp'):
+                 wav_scp = 'wav.scp', 
+                 reco2file_and_channel = 'reco2file_and_channel', 
+                 utt2spk = 'utt2spk', 
+                 spk2utt = 'spk2utt', 
+                 feats_scp = 'feats.scp', 
+                 cmvn_scp = 'cmvn.scp'):
         
         self.segments_file = segment_path + '/' + segments
         self.text_file = segment_path + '/' + text
-        self.wav_file = segment_path + '/' + wav
-        self.subset = subset
-        self._segments = pd.read_csv(self.segments_file, 
-            sep=' ', names=['wavID', 'start', 'end'], index_col=0)
-        self._text = None
-        self._wav = None
-        
+        self.reco2file_and_channel_file = segment_path + '/' + reco2file_and_channel
+        self.wav_scp_file = segment_path + '/' + wav_scp
+        self.utt2spk_file = segment_path + '/' + utt2spk
+        self.spk2utt_file = segment_path + '/' + spk2utt
+        self.feats_scp_file = segment_path + '/' + feats_scp
+        self.cmvn_scp_file = segment_path + '/' + cmvn_scp
+
+        # wav_scp MUST be present. Define recID
+        self._wav_scp = self.readtxt(self.wav_scp_file, two_cols=True, names=['recID', 'wavfile'], index_col='recID')
+
+        # segments file is OPTIONAL. Define uttID
+        try: 
+            self._segments = self.readtxt(self.segments_file, names=['uttID', 'recID', 'begin', 'end'], index_col='uttID')
+            self.uttID = self._segments.index.to_series()
+            self.recID = self._segments['recID']
+        except ValueError: 
+            self.recID = self._wav_scp.index.to_series()
+            self.recID.name = 'uttID'
+            self.uttID = self.recID
+
+        #self.uttID
+
+    # The all-important readtxt function 
+    def readtxt(self, txtfile, two_cols=False, **kwargs): 
+        if os.path.exists(txtfile): 
+            if two_cols: 
+                pipefile = pipe2file(['sed', 's/ /\t/', txtfile])
+                return pd.read_csv(pipefile, sep='\t', header=None, **kwargs)
+            else: 
+                return pd.read_csv(txtfile, delim_whitespace=True, header=None, **kwargs)
+        else: 
+            raise ValueError(txtfile + ' does not exist.')
+
+    # Various files read as pd.DataFrame
     @property
-    def segments(self):
-        return self._subset(self._segments)
+    def _text(self): 
+        return self.readtxt(self.text_file, two_cols=True, names=['uttID', 'text'], index_col='uttID')
+
+    @property
+    def _reco2file_and_channel(self): 
+        return self.readtxt(self.reco2file_and_channel_file, names=['recID', 'sphfile', 'rec_side'], index_col='recID')
+
+    @property
+    def _utt2spk(self):
+        return self.readtxt(self.utt2spk_file, names=['uttID', 'spkID'], index_col='uttID')
+
+    @property
+    def _spk2utt(self): 
+        return self.readtxt(self.spk2utt_file, two_cols=True, names=['spkID', 'uttIDs'], index_col='spkID')
+
+    @property
+    def _feats_scp(self):
+        return self.readtxt(self.feats_scp_file, two_cols=True, names=['uttID', 'featsfile'], index_col='uttID')
     
     @property
+    def _cmvn_scp(self):
+        return self.readtxt(self.cmvn_scp_file, two_cols=True, names=['spkID', 'cmvn_feats'], index_col='spkID')
+    
+    # Various columns from the files as pd.Series (all indexed by uttID)
+    @property
     def text(self): 
-        if self._text is None: 
-            pipefile = pipe2file(['sed', 's/ /\t/', self.text_file])
-            self._text = pd.read_csv(pipefile, sep='\t', 
-                names=['trans'], index_col=0)
-        return self._subset(self._text)
-        
-    @property
-    def wav(self): 
-        if self._wav is None: 
-            pipefile = pipe2file(['sed', 's/ /\t/', self.wav_file])
-            self._wav = pd.read_csv(pipefile, sep='\t', 
-                names=['wavfile'], index_col=0)
-        return self._wav.loc[self._subset(self.segments['wavID']),:]
-            
-    def _subset(self, obj): 
-        if self.subset is None: 
-            return obj
-        else: 
-            if type(obj) is pd.Series: 
-                return obj.loc[self.subset]
-            elif type(obj) is pd.DataFrame: 
-                return obj.loc[self.subset, :]
-            else: 
-                return ValueError("obj must be a pandas Series or DataFrame.")
-            
-    @property
-    def trans(self): 
-        return self.text['trans']
+        return self._text.loc[self.uttID, 'text']
         
     @property
     def wavfile(self): 
-        return self.wav['wavfile']
+        res = self._wav_scp.loc[self.recID, 'wavfile']
+        res.index = self.uttID
+        return res
         
     @property
-    def segmentID(self): 
-        return self._subset(self.segments.index.to_series())
-        
-    @property
-    def wavID(self):
-        return self.segments['wavID']
-        
-    @property
-    def start(self):
-        return self.segments['start']
+    def begin(self):
+        return self._segments.loc[self.uttID, 'begin']
         
     @property
     def end(self):
-        return self.segments['end']
+        return self._segments.loc[self.uttID, 'end']
+    
+    @property
+    def sphfile(self):
+        res = self._reco2file_and_channel.loc[self.recID, 'sphfile']
+        res.index = self.uttID
+        return res
+    
+    @property
+    def rec_side(self):
+        res = self._reco2file_and_channel.loc[self.recID, 'rec_side']
+        res.index = self.uttID
+        return res
+    
+    @property
+    def spkID(self):
+        return self._utt2spk.loc[self.uttID, 'spkID']
+    
+    @property
+    def featsfile(self):
+        return self._feats_scp.loc[self.uttID, 'featsfile']
+    
+    @property
+    def cmvn_feats(self):
+        res = self._cmvn_scp.loc[self.spkID, 'cmvn_feats']
+        res.index = self.uttID
+        return res
     
     def df(self, list_of_vars): 
         l = []
         for i in list_of_vars: 
             l = l + [self.__getattribute__(i)]
-        return pd.concat(l, axis=1)
+        return pd.concat(l, axis=1, join='inner')
 
-# path = '/home/tshmak/WORK/Projects/WFST/cmhkdemo1/data/cmhk_3rd_batch_cv'
-# s = kaldi_segment(path)
-# ss = s.segments.index[0:3].to_series()
-# s.subset = ss
-# s.start
-# s.wav
-# # s.subset = None
-# # s.end
-# # s.segmentID
-# # s.subset = ss
-# print(pd.concat([s.start, s.end], axis=1).to_csv(sep='\t', header=False))
-# s.df(['start', 'wavID'])
+
+ #path = '/home/tshmak/WORK/Projects/WFST/cmhkdemo1/data/cmhk_3rd_batch_test'
+ #s = kaldi_audio(path)
