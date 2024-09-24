ea28b8c1ec4c61303154b26519f2e55b607bdcf3
tshmak <timothy@fano.ai>
2024-09-24 10:29:30 +0800

    Minor fix to conda_tmiscpy.sh

--- conda_tmiscpy.sh
+++ conda_tmiscpy.sh
@@ -1,4 +1,4 @@
 # You can try sourcing the following, but maybe safer to run them line-by-line by copy and paste
-conda create --prefix conda_tmiscpy python=3.10
+mamba create -y --prefix conda_tmiscpy python=3.10
 conda activate ./conda_tmiscpy
 pip install -e .
